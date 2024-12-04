36ffa11f94cff72773d012ae54e3a38d525f5e91
Avantika2322 <raoavantika51@gmail.com>
2024-12-04 22:29:56 +0530

    product detail screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_app/global_data.dart';
 import 'package:shopping_app/home_page.dart';
+import 'package:shopping_app/product_details.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,7 +24,15 @@ class MyApp extends StatelessWidget {
           hintStyle: TextStyle(fontWeight: FontWeight.w400, fontSize: 16),
           prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
         ),
+        appBarTheme: const AppBarTheme(
+          titleTextStyle: TextStyle(
+              fontSize: 20, color: Colors.black, fontWeight: FontWeight.w500),
+        ),
         textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 30,
+          ),
           titleMedium: TextStyle(
             fontWeight: FontWeight.bold,
             fontSize: 20,
@@ -34,7 +44,8 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+     // home: const HomePage(),
+      home: ProductDetails(product: products[0],),
     );
   }
 }
