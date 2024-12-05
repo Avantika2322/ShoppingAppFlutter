df86635d639a771c89ce682415b13c1d9111c984
avantikarao <avantika.rao@agicent.in>
2024-12-05 18:57:37 +0530

    product detail

--- lib/product_details.dart
+++ lib/product_details.dart
@@ -1,26 +1,98 @@
 import 'package:flutter/material.dart';
 
-class ProductDetails extends StatelessWidget {
+class ProductDetails extends StatefulWidget {
   final Map<String, dynamic> product;
+
   const ProductDetails({super.key, required this.product});
+
+  @override
+  State<ProductDetails> createState() => _ProductDetailsState();
+}
+
+class _ProductDetailsState extends State<ProductDetails> {
+  int selectedSize = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Details'),
+        title: const Text(
+          'Details',
+          textAlign: TextAlign.center,
+        ),
       ),
       body: Column(
         children: [
           Text(
-            product['title'] as String,
+            widget.product['title'] as String,
             style: Theme.of(context).textTheme.titleLarge,
+            textAlign: TextAlign.center,
           ),
           const Spacer(),
-          Image.asset(product['imageUrl']),
+          Image.asset(widget.product['imageUrl']),
           const Spacer(
             flex: 2,
           ),
+          Container(
+            height: 250,
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(245, 247, 249, 1),
+              borderRadius: BorderRadius.circular(40),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  '\$${widget.product['price']}',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                const SizedBox(),
+                SizedBox(
+                  height: 50,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: widget.product['sizes'].length,
+                    itemBuilder: (context, index) {
+                      final int size = widget.product['sizes'][index];
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: GestureDetector(
+                          onTap: (){
+                            setState(() {
+                              selectedSize =size;
+                            });
+                          },
+                          child: Chip(
+                            label: Text(size.toString()),
+                            backgroundColor: selectedSize == size
+                                ? const Color.fromRGBO(254, 206, 1, 1)
+                                : const Color.fromRGBO(245, 247, 249, 1),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromRGBO(254, 206, 1, 1),
+                      minimumSize: const Size(double.infinity, 50),
+                    ),
+                    child: const Text(
+                      'Add To Cart',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
